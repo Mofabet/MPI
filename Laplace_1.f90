c80d1cc9dff8292a66de8417815d14ed1e6a79a5
Mofabet <gutamaya@outlook.com>
2020-04-28 15:59:00 +0500

    NE_INT

--- Laplace_1.f90
+++ Laplace_1.f90
@@ -3,7 +3,7 @@ program Laplace
   implicit none
   include 'mpif.h'
   INTEGER :: ERR, SIZE, RANK, ST(MPI_STATUS_SIZE)
-  integer :: i, j, n, m, s, c_1, c_2, c_3, duck_1, k, top, bottom, iter, iterrations, int, mrbin
+  integer :: i, j, n, m, s, c_1, c_2, c_3, duck_1, k, top, bottom, iter, iterrations, NE_INT, mrbin
   integer, allocatable ::  MBAND(:)
   real :: eps, err_0, error, dt1, dt2
   double precision, allocatable :: A(:,:), B(:,:), C(:,:), D(:,:), E(:,:), iBAND(:,:)
@@ -14,7 +14,7 @@ call MPI_INIT(ERR)
 call MPI_COMM_SIZE(MPI_COMM_WORLD, SIZE, ERR)
 call MPI_COMM_RANK(MPI_COMM_WORLD, RANK, ERR)
 
-if (RANK .eq. 0) then
+if (RANK .eq. 0) then !0000000000
   write (6,*)'Initialization of the program'
   write (6,*)'Enter the required accuracy:'
   read(*,*)eps
@@ -39,7 +39,7 @@ if (RANK .eq. 0) then
   write(6,*) 'Number of iterrations ='
   read(*,*) iterrations
 write(6,*)'--------------------------------------------------------------------'
-endif
+endif !0000000000
   !open(10, file = 'A', form = 'formatted', status = 'unknown')
   !read(10,*)n1
   !read(10,*)m1
@@ -49,7 +49,7 @@ endif
 !  do i=1,n
 !      read(10,*)(A(i,j), j=1,m)
 !  enddo
-if (rank.eq.0) then
+if (rank.eq.0) then !0000000000
 
 dt1=(tm(1,m)-tm(1,1))/m
 dt2=(tm(n,m)-tm(n,1))/m
@@ -98,25 +98,25 @@ allocate(disp(SIZE))
 !-------------------------------------B----------------------------------------!
 duck_1 = disp(1)-2
     do c_1 = 1, SIZE - 1
-      int = row(i + 1)
-      allocate(iBAND(int,m))
-      do c_2 = 1, int!m     !do c_1 = 1,iBAND
+      NE_INT = row(i + 1)
+      allocate(iBAND(NE_INT,m))
+      do c_2 = 1, NE_INT!m     !do c_1 = 1,iBAND
         do c_3 = 1, m
             iBAND(c_2,c_3) = tm(c_1+c_2+ duck_1, c_3) !заполнение, c_1 i+j !!!!!!!!!!!!
         enddo
       enddo
 !2 3 svobodni
 duck_1 = duck_1 + disp(c_1 + 1) - 1
-      call MPI_SEND(iBAND, int*m,MPI_DOUBLE_PRECISION,c_1,20+c_1,MPI_COMM_WORLD,ERR)!mmm*c
+      call MPI_SEND(iBAND, NE_INT*m,MPI_DOUBLE_PRECISION,c_1,20+c_1,MPI_COMM_WORLD,ERR)!mmm*c
       write(6,*)'Band on ',c_1,'=='
-      do c_2=1, int
+      do c_2=1, NE_INT
         write(6,'(100f8.3)')(iBAND(c_2,c_3),c_3 =1,m)
       enddo
       deallocate (iBAND)!---
       !duck_2 = duck_2 + 1
       !write(6,*),duck_2,RANK
   enddo !68 79
-endif
+endif  !0000000000
 !if (RANK .ne. 0) then
 !  allocate(row(SIZE))
 !endif
@@ -129,27 +129,27 @@ CALL MPI_BCAST(row, SIZE, MPI_DOUBLE_PRECISION, 0, MPI_COMM_WORLD, err)
 !ALL MPI_BCAST(MBAND, SIZE, MPI_DOUBLE_PRECISION, 0, MPI_COMM_WORLD,ERR)
 !allocate(row(SIZE))
 
-if (RANK .eq. 0) then
-  int = row(1)
-  allocate(iBAND(int,m)) !tut tozhe ne ponyatno nm int 1 ili 2 s por-m...
-  do c_1 = 1, int
+if (RANK .eq. 0) then  !0000000000
+  NE_INT = row(1)
+  allocate(iBAND(NE_INT,m)) !tut tozhe ne ponyatno nm NE_INT 1 ili 2 s por-m...
+  do c_1 = 1, NE_INT
     do c_2 = 1, m
       iBAND(c_1,c_2) = tm(c_1,c_2)
     enddo
-  enddo
+  enddo  !0000000000
 
 
   !do
   !enddo
-else !----------------
-  int = row(RANK + 1)
-  allocate(iBAND(int,m), t(int,m))
-  CALL MPI_RECV(iBAND, int*m,MPI_DOUBLE_PRECISION,0,20+RANK,MPI_COMM_WORLD,ST,ERR) !т/т
-  endif
+else !---------------- !1231231313131313131
+  NE_INT = row(RANK + 1)
+  allocate(iBAND(NE_INT,m), t(NE_INT,m))
+  CALL MPI_RECV(iBAND, NE_INT*m,MPI_DOUBLE_PRECISION,0,20+RANK,MPI_COMM_WORLD,ST,ERR) !т/т
+  endif !1231231313131313131
 
   !http://www2.sscc.ru/Publikacii/Primery_Prll/1-4.htm
 !SDVIG
-  if (RANK .eq. 0) then
+  if (RANK .eq. 0) then !0000000000
     top = MPI_PROC_NULL                            !VERH
   else
     top = RANK - 1
@@ -170,9 +170,9 @@ do iter = 1, iterrations
 err_0 = 0
 error = 0
 
-int = row(RANK + 1)
+NE_INT = row(RANK + 1)
 
-do c_1 = 2, (int - 1)
+do c_1 = 2, (NE_INT - 1)
   do c_2 = 2, (m - 1)          !  1                    2                      3                    4
     T(c_1,c_2) = ((iBAND(c_1 - 1,c_2) + iBAND(c_1 + 1,c_2) + iBAND(c_1,c_2 - 1) + iBAND(c_1,c_2 + 1))/4)
     err_0 = err_0 + (t(c_1,c_2) - iBAND(c_1,c_2))**2
@@ -182,9 +182,9 @@ enddo
 iBAND = T
 
 CALL MPI_SENDRECV(T(2,:), m, MPI_DOUBLE_PRECISION, top, RANK+(iter*(iter + 100)+iter), & !тег надо заменить, если лимит будет привышен
-iBAND(int, :), m, MPI_DOUBLE_PRECISION, bottom, (RANK + 1)+(iter*(iter + 100)+iter), & !можно повысить степень ранка, а ост оставить
+iBAND(NE_INT, :), m, MPI_DOUBLE_PRECISION, bottom, (RANK + 1)+(iter*(iter + 100)+iter), & !можно повысить степень ранка, а ост оставить
 MPI_COMM_WORLD, ST, ERR)
-CALL MPI_SENDRECV(T(int - 1,:), m, MPI_DOUBLE_PRECISION, bottom,  RANK+(iter*(iter+999)+iter), &
+CALL MPI_SENDRECV(T(NE_INT - 1,:), m, MPI_DOUBLE_PRECISION, bottom,  RANK+(iter*(iter+999)+iter), &
 IBAND(1,:), m,MPI_DOUBLE_PRECISION,top,  (RANK - 1)+(iter*(iter + 999)+iter), &
 MPI_COMM_WORLD, ST, ERR)
 
@@ -201,14 +201,14 @@ write(6,*) RANK,' = ', iBAND
 
 if (RANK .eq. 0) then
   allocate(out(n,m))
-  do c_1 = 1, int - 1 !perenos
+  do c_1 = 1, NE_INT - 1 !perenos
     do c_2 = 1, m
       out(c_1,c_2) = iBAND (c_1,c_2)
     enddo
   enddo
 deallocate(iBAND)
 else
-  call MPI_SEND(iBAND, int*m, MPI_DOUBLE_PRECISION, 0, 10+RANK, MPI_COMM_WORLD, ERR)
+  call MPI_SEND(iBAND, NE_INT*m, MPI_DOUBLE_PRECISION, 0, 10+RANK, MPI_COMM_WORLD, ERR)
 endif
 
 if (RANK .eq. 0) then
@@ -219,10 +219,10 @@ if (RANK .eq. 0) then
     else
       mrbin = 1
     endif
-    int = row (c_1 + 1)
-    allocate(iBAND(int,m))
-    CALL MPI_RECV(iBAND, int*m, MPI_DOUBLE_PRECISION, c_1, 10+c_1, MPI_COMM_WORLD, ST, ERR)
-    do c_2 = 2, (int - mrbin) !!!!!!!!!!!
+    NE_INT = row (c_1 + 1)
+    allocate(iBAND(NE_INT,m))
+    CALL MPI_RECV(iBAND, NE_INT*m, MPI_DOUBLE_PRECISION, c_1, 10+c_1, MPI_COMM_WORLD, ST, ERR)
+    do c_2 = 2, (NE_INT - mrbin) !!!!!!!!!!!
       do c_3 = 1, m
         out(c_2 + duck_1 - 1, c_3) = iBAND(c_2,c_3)
       enddo
