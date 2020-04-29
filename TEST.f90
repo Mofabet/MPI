b809a698daa03b243410f1f15e701448f54e82fc
Mofabet <gutamaya@outlook.com>
2020-04-29 13:58:50 +0500

    Update TEST.f90

--- TEST.f90
+++ TEST.f90
@@ -2,10 +2,8 @@ program TEST
 implicit none
 include 'mpif.h'
 INTEGER :: ERR, SIZE, RANK, ST(MPI_STATUS_SIZE)
-integer :: i, j, n1, n2, m1, m2, c_1, c_2, c_3, c_4, duck_1, duck_2, iter
-integer, allocatable ::  MBAND(:), disp(:)
+integer :: i, j, n1, n2, m1, m2, c_1, c_2, c_3, c_4, duck_1, duck_2
 double precision, allocatable :: A(:,:), B(:,:), C(:,:), D(:,:), E(:,:), iBAND(:,:)
-double precision, allocatable :: X(:), X_0(:), X_1(:), g(:), g_0(:), tmp_alloc(:)
 double precision :: tmp, eps, err_0, error, time, time_1, time_2
 
 !  3. Прочитать на «0» процессе из файла А массив NxN (размерность массива N записана в первой строке файла А).
